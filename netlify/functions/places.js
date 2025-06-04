83359f65b4409ce3bfd165cab84fd397527960c4
Gerhard <sailbuddy2me@gmail.com>
2025-06-04 17:30:14 +0200

    🔧 Fix: use GITHUB_TOKEN_PLAIN from Netlify env

--- netlify/functions/places.js
+++ netlify/functions/places.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN_PLAIN;
 
 exports.handler = async function (event) {
   const headers = {
